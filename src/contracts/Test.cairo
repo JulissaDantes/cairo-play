2d2fe2b28ff99e1bff561c78fa9668f7f123a28d
JulissaDantes <julissadcj@gmail.com>
2022-06-07 13:00:16 +0000

    Add new library

--- src/contracts/Test.cairo
+++ src/contracts/Test.cairo
@@ -4,10 +4,10 @@
 %lang starknet
 
 from starkware.cairo.common.cairo_builtins import HashBuiltin, SignatureBuiltin, BitwiseBuiltin
-from starkware.cairo.common.cairo_secp.signature import verify_eth_signature_uint256, public_key_point_to_eth_address
+from starkware.cairo.common.cairo_secp.signature import verify_eth_signature_uint256, public_key_point_to_eth_address, recover_public_key
 from starkware.cairo.common.alloc import alloc
 from starkware.cairo.common.cairo_secp.ec import EcPoint
-from starkware.cairo.common.cairo_secp.bigint import BigInt3
+from starkware.cairo.common.cairo_secp.bigint import BigInt3, uint256_to_bigint, bigint_to_uint256
 from starkware.cairo.common.uint256 import Uint256
 
 @external
@@ -37,21 +37,35 @@ func _verify_eth_signature_uint256{
 
         return ()
 end
-
+@external
 func get_key{
             syscall_ptr : felt*,
             pedersen_ptr : HashBuiltin*,
             range_check_ptr,
             bitwise_ptr: BitwiseBuiltin*
         }(
-        x: BigInt3,
-        y: BigInt3
+        ux: Uint256,
+        uy: Uint256
         ) -> (eth_address: felt):
         alloc_locals  
-        
+        let x : BigInt3 = uint256_to_bigint(ux)
+        let y : BigInt3 = uint256_to_bigint(uy)
         let (local keccak_ptr : felt*) = alloc()
         let keccak_ptr_start = keccak_ptr
         let point : EcPoint = EcPoint(x=x,y=y)
         let eth_address : felt = public_key_point_to_eth_address{keccak_ptr=keccak_ptr}(point)
         return (eth_address)
-        end
+    end
+
+    @external
+    func _recover_public_key{range_check_ptr}(
+    umsg_hash : Uint256, ur : Uint256, us : Uint256, v : felt
+    ) -> (x : Uint256, y: Uint256):
+        let r : BigInt3 = uint256_to_bigint(ur)
+        let s : BigInt3 = uint256_to_bigint(us)
+        let msg_hash : BigInt3 = uint256_to_bigint(umsg_hash)
+        let (public_key_point : EcPoint) = recover_public_key(msg_hash,r,s,v)
+        let x: Uint256 = bigint_to_uint256(public_key_point.x)
+        let y: Uint256 = bigint_to_uint256(public_key_point.y)
+        return (x, y)
+    end
--- tests/test_test.py
+++ tests/test_test.py
@@ -10,15 +10,23 @@ import eth_keys
 import sys
 from nile.signer import Signer, from_call_to_call_array, get_transaction_hash
 
-pk = eth_keys.keys.PrivateKey(b'\x01' * 32)        
+private_key = b'\x01' * 32
+pk = eth_keys.keys.PrivateKey(private_key)        
 pubk = pk.public_key.to_checksum_address()
+
 
 IACCOUNT_ID = 0xf10dbd44
 TRUE = 1
 
 def signing(account):
     message_to_hash = b'testing a message hash'
-    hash = get_transaction_hash(account.contract_address, [], [], 0, 0)
+    #must get account, call_array, calldata, nonce, max_fee
+    hash = get_transaction_hash(account.address, [], [], 0, 0)
+    print(hash,'im getiing this as a hashy''all')
+    #2828821075934055852220125569300886328237608189494176769220760037777757219766
+    #of course is different Iam pasing a changing param
+    #todo revisit original implementatiomn
+    #with static param: 2756949966536968755053797530078455186503706923552182306731355083062150328381
     signature = pk.sign_msg_hash(message_to_hash)
     sig_r = to_uint(signature.r)
     sig_s = to_uint(signature.s)
@@ -43,11 +51,36 @@ async def account_factory():
 async def test_constructor(account_factory):
     account = account_factory
     hash, v, r, s = signing(account)
-    #hash = 
-    print(hash,type(hash), int(pubk,0), pubk)
+    x = to_uint(int(pk.public_key[:32].hex(),16))
+    y = to_uint(int(pk.public_key[32:].hex(),16))
+    #int(pubk,0)this is the right way to get the eth address
+    #print( hash, r, s, v)
+    #3390502374062641025540663937380216726131919521463450836644846335041785841004 (70967922200151345553514533283644799340, 300747944610989563154359195732535743235) (196764562288776768217680723670754120955, 92388767693770458896504210395837532470) 0
+    #print( int(pubk,0), pk.public_key, pubk)
+    #public = await account.get_key(x, y).invoke()
+    #print(public.result)
+    test_exec = await account._recover_public_key(to_uint(hash), r, s, v).invoke()#This is not returning the right thing
+    rx = from_uint(test_exec.result.x)
+    ry = from_uint(test_exec.result.y)
+    print('this',from_uint(x),from_uint(y))
+    print('resulting in', rx, ry)
     execution_info = await account._verify_eth_signature_uint256(int(pubk,0), to_uint(hash), v, r, s).invoke()
     assert execution_info.result == (signer.public_key,)
-    #150667933682724627262632139063411478963274978545 != 293718746239343348680605004835135740192371701289 son las eth addresses
+    #150667933682724627262632139063411478963274978545 != this is the one i got
+    #293718746239343348680605004835135740192371701289 son las eth addresses que siempre son diferentes
+    #341450081602188049170303368647362638690376738397
+
+
+#_recover_public_key returns a diferent point everythime
+#EcPoint(
+#x=BigInt3(d0=42355523410962634072598265, d1=54489418108100514937072803, d2=8284547590306369974861239), 
+#y=BigInt3(d0=35517114584224975643378862, d1=32253362464838751594470283, d2=8216016518097359459525859))
+#)
+
+def from_uint(uint):
+    """Takes in uint256-ish tuple, returns value."""
+    return uint[0] + (uint[1] << 128)
+
 
 def to_uint(a):
     """Takes in value, returns uint256-ish tuple."""
